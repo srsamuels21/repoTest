85073bb02aa969f0c9996e592bb1d5316adb9695
srsamuels21 <61839632+srsamuels21@users.noreply.github.com>
2020-05-19 12:41:18 -0500

    numbers

--- testExtension2.js
+++ testExtension2.js
@@ -2,7 +2,7 @@ var variable = true;
 variable == true ? console.log("hello") : false;
 
 //this comment is here for review
-var red = "red";
-var blue = "blue";
+var red = "100";
+var blue = "1000";
 
 red == blue ? console.log("purple") : false;
